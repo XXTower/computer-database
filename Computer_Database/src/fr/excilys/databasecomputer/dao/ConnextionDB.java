0b29fff1b09f9b63150a0f17c1f54d5d78d837d0
excilys <excilys@Laptop-0193>
2019-10-08 13:55:22 +0200

    Change connection for possibility to close

--- Computer_Database/src/fr/excilys/databasecomputer/dao/ConnextionDB.java
+++ Computer_Database/src/fr/excilys/databasecomputer/dao/ConnextionDB.java
@@ -6,20 +6,25 @@ import java.sql.SQLException;
 
 public class ConnextionDB {
 
-	private static String url = "jdbc:mysql://localhost:3306/computer-database-db";
+	private static String url = "jdbc:mysql://localhost:3306/computer-database-db?useSSL=false";
 	private static String user = "admincdb";
 	private static String password = "qwerty1234";
 	private static Connection connect;
 	
-	//retour une nouvelle instance de BDD si n'a pas déja été crer 
 	public static Connection getInstance() {
-		if(connect == null) {
-			try {
-				connect = DriverManager.getConnection(url, user, password);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+		try {
+			connect = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		return connect;
+	}
+	
+	public static void closeConnection() {
+		try {
+			connect.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
