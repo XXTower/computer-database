6657e3ebbdd54635662bdfcf0f305da3874a90b2
excilys <excilys@Laptop-0193>
2019-10-15 16:45:35 +0200

    Start to add test for company DAO

---
+++ src/test/java/fr/excilys/databasecomputer/dao/implment/CompanyDaoTest.java
@@ -0,0 +1,30 @@
+package fr.excilys.databasecomputer.dao.implment;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class CompanyDaoTest extends TestCase {
+
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public final void testDisplayAll() {
+		fail();
+	}
+	
+	@Test
+	public final void testNbCompany() {
+		fail();
+	}
+}
