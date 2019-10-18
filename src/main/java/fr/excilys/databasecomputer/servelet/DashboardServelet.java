7cb5768b157b1477a096c63835f425a94a6e83b5
excilys <excilys@Laptop-0193>
2019-10-18 12:08:54 +0200

    Change name Servlet

--- src/main/java/fr/excilys/databasecomputer/servelet/DashboardServelet.java
+++ src/main/java/fr/excilys/databasecomputer/servelet/DashboardServelet.java
@@ -8,15 +8,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/dashboard")
-public class Dashboard extends HttpServlet {
+public class DashboardServelet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public Dashboard() { }
+    public DashboardServelet() { }
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int test = 7;
+		request.setAttribute("nb_computer", test);
 		this.getServletContext().getRequestDispatcher("/views/dashboard.jsp").forward(request, response);
 	}
 
